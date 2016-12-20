428b860ed768b43d02ec798bd321d9a12b9311d6
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-12-19 22:35:44 -0800

    enter the webpack module bundler

---
+++ enter-webpack/webpack.config.js
@@ -0,0 +1,6 @@
+module.export = {
+  entry: './i-want-these-files/can-we-just-do-like-node.js',
+  output: {
+    filename: './i-dont-wanna-have-to-worry-about-the-damn-loading-order/ahh-yeah.js'
+  }
+}
