3955a2ebcaf9aa8dc745aabe5ea772997cc2ac34
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-12-29 21:16:20 -0800

    (update) #5 update webpack-config finish branch

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let chunk = require('lodash').chunk;
+const greeting = './file2.js';
 
-console.log(`hello from ${require('./file2.js')}`);
+const chunks = chunk(['1', '2', '3', '4'], 2);
+
+console.log(`hello from ${greeting}`);
+console.log(`checkout these chunks: ${chunks[0]} and ${chunks[1]}`);
